2e491b832b884086335a8681099bdee1be473ccf
Charles AZAM <azamcharles0@gmail.com>
2025-08-29 18:36:20 +0200

    Fix main (#103)
    * fix: test invest
    
    * feat: run all tests
    
    * feat: run all tests
    
    * fix: timezone

--- predibench-core/tests/test_invest.py
+++ predibench-core/tests/test_invest.py
@@ -2,48 +2,56 @@ from datetime import date, timedelta
 
 from predibench.common import DATA_PATH
 from predibench.invest import run_investments_for_specific_date
-from smolagents.models import InferenceClientModel
+from predibench.models import ModelInfo
 
 
 def test_invest():
     models = [
-        InferenceClientModel(model_id="openai/gpt-oss-120b"),
+        ModelInfo(
+            model_id="openai/gpt-oss-120b",
+            model_pretty_name="GPT-OSS 120B",
+            inference_provider="fireworks-ai",
+            company_pretty_name="OpenAI",
+            open_weights=True,
+            agent_type="toolcalling",
+        ),
     ]
-    target_date = date(2025, 7, 16)
 
-    result = run_investments_for_specific_date(
-        time_until_ending=timedelta(days=21),
-        max_n_events=3,
+    results = run_investments_for_specific_date(
         models=models,
-        output_path=DATA_PATH,
-        target_date=target_date,
+        time_until_ending=timedelta(days=7 * 6),
+        max_n_events=2,
+        target_date=date(2025, 8, 25),
     )
 
-    assert isinstance(result, list)
-    if len(result) > 0:
-        assert hasattr(result[0], "model_id")
-        assert hasattr(result[0], "target_date")
-
-
-def test_invest_backward():
+def test_invest_without_cache():
     models = [
-        InferenceClientModel(model_id="openai/gpt-oss-120b"),
+        ModelInfo(
+            model_id="openai/gpt-oss-120b",
+            model_pretty_name="GPT-OSS 120B",
+            inference_provider="fireworks-ai",
+            company_pretty_name="OpenAI",
+            open_weights=True,
+            agent_type="toolcalling",
+        ),
     ]
     target_date = date(2025, 7, 16)
 
     result = run_investments_for_specific_date(
         time_until_ending=timedelta(days=21),
-        max_n_events=3,
+        max_n_events=1,
         models=models,
-        output_path=DATA_PATH,
         target_date=target_date,
+        force_rewrite=True,
     )
 
     assert isinstance(result, list)
     if len(result) > 0:
         assert hasattr(result[0], "model_id")
         assert hasattr(result[0], "target_date")
+
 
 
 if __name__ == "__main__":
-    test_invest_backward()
+    test_invest()
+    test_invest_without_cache()
